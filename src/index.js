f80356e7b20392ba2c105ad54f58e2de4f4c4c25
Vishal <singhdhamivishal@gmail.com>
2022-12-12 21:21:53 +0530

    initialsetupchanges

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
 const setupAndStartServer = async () => {
+  
+    //create the express object 
   const app = express();
-
   app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
   });
